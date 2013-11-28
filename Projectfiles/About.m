727b4635631dc7a22637255654ebb67711d23252
Shalin Shah <shalinvs@gmail.com>
2013-11-28 01:03:27 -0800

    More stuff in the About page

--- Projectfiles/About.m
+++ Projectfiles/About.m
@@ -7,6 +7,7 @@
 //
 
 #import "About.h"
+#import "Settings.h"
 
 @implementation About
 
@@ -17,32 +18,58 @@
         // Some variables to make positioning more easy
         size = [[CCDirector sharedDirector] winSize];
         screenCenter = ccp(size.width/2, size.height/2);
-        
-        CCLabelTTF *settings = [CCLabelTTF labelWithString:@"ABOUT" fontName:@"HelveticaNeue-UltraLight" fontSize:55];
-        settings.position = ccp(screenCenter.x, screenCenter.y * 1.7);
-        [self addChild:settings];
-        
+
         CCLayerColor* colorLayer = [CCLayerColor layerWithColor:ccc4(255, 255, 255, 255)];
         [self addChild:colorLayer z:0];
         
-//        CCMenuItemImage *about = [CCMenuItemImage itemWithNormalImage:@"about.png" selectedImage:@"about.png" target:self selector:@selector(resetGameData)];
-        
-        CCSprite *about = [CCSprite spriteWithFile:@"about.png"];
+        CCSprite *about = [CCSprite spriteWithFile:@"me.png"];
         [self addChild:about z:1];
         about.position = screenCenter;
         
+        CCMenuItemImage *facebook = [CCMenuItemImage itemWithNormalImage:@"facebook.png" selectedImage:@"facebook.png" target:self selector:@selector(facebook)];
+        facebook.scale = 0.6f;
+        
+        CCMenuItemImage *twitter = [CCMenuItemImage itemWithNormalImage:@"twitter.png" selectedImage:@"twitter.png" target:self selector:@selector(twitter)];
+        twitter.scale = 0.6f;
+        
+        CCMenuItemImage *email = [CCMenuItemImage itemWithNormalImage:@"email.png" selectedImage:@"email.png" target:self selector:@selector(email)];
+        email.scale = 0.6f;
+        
+        CCMenu *menu = [CCMenu menuWithItems:facebook, twitter, email, nil];
+        [menu alignItemsHorizontallyWithPadding:8];
+        menu.position = ccp(screenCenter.x,screenCenter.y * 1.4);
+        [self addChild:menu];
+        
+        CCMenuItemImage *back = [CCMenuItemImage itemWithNormalImage:@"arrow.png" selectedImage:@"arrow-sel.png" target:self selector:@selector(back)];
+        back.scale = 1.1f;
+        CCMenu *backMenu = [CCMenu menuWithItems:back, nil];
+        backMenu.position = ccp(screenCenter.x,screenCenter.y / 3);
+        [self addChild:backMenu];
         
     }
     return self;
 }
 
 
+-(void) facebook {
+    NSURL *facebookURL = [NSURL URLWithString:@"https://www.facebook.com/getcenterapp"];
+    [[UIApplication sharedApplication] openURL:facebookURL];
+    
+}
 
+-(void) twitter {
+    NSURL *twitterURL = [NSURL URLWithString:@"https://twitter.com/getcenter"];
+    [[UIApplication sharedApplication] openURL:twitterURL];
+}
 
+-(void) email {
+    NSURL *emailURL = [NSURL URLWithString:@"mailto:shalinvs@gmail.com"];
+    [[UIApplication sharedApplication] openURL:emailURL];
+}
 
-
-
-
+-(void) back {
+    [[CCDirector sharedDirector] replaceScene: [CCTransitionProgress transitionWithDuration:0.5f scene:[Settings node]]];
+}
 
 
 @end
